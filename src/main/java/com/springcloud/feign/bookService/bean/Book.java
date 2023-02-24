8aabff990a1d33cb297956f56d9947a4be475347
Venkat <tech.venkat91@gmail.com>
2023-02-24 11:55:35 +0530

    modified the controler classes

--- src/main/java/com/springcloud/feign/bookService/bean/Book.java
+++ src/main/java/com/springcloud/feign/bookService/bean/Book.java
@@ -11,31 +11,4 @@ public class Book {
 	private Integer bookId;
 	private String bookName;
 	private double bookCost;
-	public Book(Integer bookId, String bookName, double bookCost) {
-		this.bookId = bookId;
-		this.bookName = bookName;
-		this.bookCost = bookCost;
 	}
-	public Integer getBookId() {
-		return bookId;
-	}
-	public void setBookId(Integer bookId) {
-		this.bookId = bookId;
-	}
-	public String getBookName() {
-		return bookName;
-	}
-	public void setBookName(String bookName) {
-		this.bookName = bookName;
-	}
-	public double getBookCost() {
-		return bookCost;
-	}
-	public void setBookCost(double bookCost) {
-		this.bookCost = bookCost;
-	}
-	public Book() {
-	}
-
-	
-}
--- src/main/java/com/springcloud/feign/bookService/controller/BookRestController.java
+++ src/main/java/com/springcloud/feign/bookService/controller/BookRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,7 @@ public class BookRestController {
 	@Autowired
 	private Environment environment;
 
+	@GetMapping("/data")
 	public String bookData() {
 		return "data on BOOK-SERVICE, Running on port:" + environment.getProperty("local.server.port");
 	}
@@ -31,5 +34,10 @@ public class BookRestController {
 	public List<Book> getAllBooks() {
 		return Arrays.asList(new Book(1, "java with Microservice", 10000.0), new Book(2, "Devops", 12000.0),
 				new Book(3, "AWS", 13000.0));
+	}
+
+	@GetMapping("/entity")
+	public ResponseEntity<String> getEntityData() {
+		return new ResponseEntity<String>("Hello from BookRestController", HttpStatus.OK);
 	}
 }
